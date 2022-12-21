c82cc895019729e95870e6c95f88698aedcc4998
Branda0 <g.brandao.dev@gmail.com>
2022-12-21 01:53:35 +0100

    puzzle 1&2 day20

--- day20/solution.js
+++ day20/solution.js
@@ -11,15 +11,27 @@ function getInputs(file) {
 
   return inputs;
 }
+
+function getInputs2(file) {
+  const decryption_key = 811589153;
+
+  const inputs = fs
+    .readFileSync(file, "utf8")
+    .split("\n")
+    .map((line, index) => ({
+      index,
+      value: line * decryption_key,
+    }));
+
+  return inputs;
+}
 Array.prototype.move = function (from, to) {
   this.splice(to, 0, this.splice(from, 1)[0]);
 };
 
 // PUZZLES 1&2
 
-function puzzle1() {
-  const encrypted_lines = getInputs("input.txt");
-
+function mix(encrypted_lines) {
   const size = encrypted_lines.length;
 
   for (index = 0; index < size; index++) {
@@ -37,6 +49,33 @@ function puzzle1() {
       );
     }
   }
+}
+
+function puzzle1() {
+  const encrypted_lines = getInputs("input.txt");
+
+  const size = encrypted_lines.length;
+
+  mix(encrypted_lines);
+
+  let index_value_0 = encrypted_lines.findIndex((line) => line.value === 0);
+
+  let res =
+    encrypted_lines[(index_value_0 + 1000) % size].value +
+    +encrypted_lines[(index_value_0 + 2000) % size].value +
+    encrypted_lines[(index_value_0 + 3000) % size].value;
+
+  return res;
+}
+
+function puzzle2() {
+  const encrypted_lines = getInputs2("input.txt");
+
+  const size = encrypted_lines.length;
+
+  for (let i = 0; i < 10; i++) {
+    mix(encrypted_lines);
+  }
 
   let index_value_0 = encrypted_lines.findIndex((line) => line.value === 0);
 
@@ -49,4 +88,4 @@ function puzzle1() {
 }
 
 console.log("Puzzle 1: " + puzzle1());
-// console.log(`Puzzle 2, ${exposed_to_air} exposed lava cube faces`);
+console.log("Puzzle 2: " + puzzle2());
